cfb7c93190f1c43c2dfa7d18a55f654d0b8651e8
niltuhon <niltuhon552@gmail.com>
2024-12-09 06:15:27 +0400

    response type fix

--- app/PostbackSms/Contracts/Number/NumberServiceInterface.php
+++ app/PostbackSms/Contracts/Number/NumberServiceInterface.php
@@ -3,6 +3,7 @@
 namespace App\PostbackSms\Contracts\Number;
 
 use App\PostbackSms\Dtos\CancelNumberResponseDto;
+use App\PostbackSms\Dtos\GetActivationStatusResponseDto;
 use App\PostbackSms\Dtos\GetNumberResponseDto;
 use App\PostbackSms\Dtos\GetSmsResponseDto;
 
@@ -11,5 +12,5 @@ interface NumberServiceInterface
     public function getNumber(string $country, string $service, string $token, ?int $rent_time): GetNumberResponseDto;
     public function cancelNumber(string $token, int $activation): CancelNumberResponseDto;
     public function getSms(string $token, int $activation): GetSmsResponseDto;
-    public function getActivationStatus(string $token, int $activation): GetSmsResponseDto;
+    public function getActivationStatus(string $token, int $activation): GetActivationStatusResponseDto;
 }
