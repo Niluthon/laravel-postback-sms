6b0101c59dc723095b9624a76c1c290fac8f24c7
niltuhon <niltuhon552@gmail.com>
2024-12-09 06:14:30 +0400

    typo fix

--- app/PostbackSms/Dtos/GetActivationStatusResponseDto.php
+++ app/PostbackSms/Dtos/GetActivationStatusResponseDto.php
@@ -5,25 +5,25 @@ namespace App\PostbackSms\Dtos;
 use App\PostbackSms\Enums\Responses\ResponseCodeEnum;
 use DateTime;
 
-readonly final class GetStatusResponseDto
+readonly final class GetActivationStatusResponseDto
 {
     public ResponseCodeEnum $code;
     public string $status;
     public int $count_sms;
-    public DateTime $end_rend_date;
+    public ?DateTime $end_rent_date;
 
     /**
      * @param ResponseCodeEnum $code
      * @param string $status
      * @param int $count_sms
-     * @param DateTime $end_rend_date
+     * @param DateTime|null $end_rent_date
      */
-    public function __construct(ResponseCodeEnum $code, string $status, int $count_sms, DateTime $end_rend_date)
+    public function __construct(ResponseCodeEnum $code, string $status, int $count_sms, ?DateTime $end_rent_date = null)
     {
         $this->code = $code;
         $this->status = $status;
         $this->count_sms = $count_sms;
-        $this->end_rend_date = $end_rend_date;
+        $this->end_rent_date = $end_rent_date;
     }
 
 
